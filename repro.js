0349d54322650fc3f57414f99eeaa8b90fcfa602
Patrick Donelan <pat@patspam.com>
2019-10-22 08:10:24 +1100

    Prettier JSX printWidth bug repro

---
+++ repro.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Foo = props => (
+  <div>
+    xxxxxxxx xxxx xxxx xx xxx xxxxxxxxXXXXXX XXXXXXX XXXXXxxxxxxxx xxxxxxx xxxxx{" "}
+  </div>
+)
+
+export default Foo
